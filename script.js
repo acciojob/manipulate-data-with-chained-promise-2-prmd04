3db31fea1fad3189b8b3b35aa4f931099bbdf82f
Pramod Ade <109603626+prmd04@users.noreply.github.com>
2025-06-18 17:44:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+let initalPromise = function() {
+    return new Promise((resolve) =>{
+        setTimeout(()=>{
+            resolve([1,2,3,4])
+        },3000)
+    });
+}
+
+let ouput = document.getElementById("output");
+
+initalPromise()
+.then((result) =>{
+    const evenNumber = result.filter((item) => item % 2===0);
+    setTimeout(()=>{
+         ouput.innerText = `${evenNumber.join(',')}`;
+    },1000)
+
+    return evenNumber;
+})
+.then((even)=>{
+    const double = even.map((item) => item*2);
+    setTimeout(()=>{
+         ouput.innerText = `${double.join(',')}`;
+    },2000)
+   
+})
+   
